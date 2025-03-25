dfb2c6d447efa0261412a8af72b0abe9e1f438f2
Manuel Carrascal <tricdiart@gmail.com>
2025-03-24 22:09:56 -0500

    First Commit

---
+++ src/main/java/com/pragma/powerup/NotificationMessagingMicroserviceApp.java
@@ -0,0 +1,13 @@
+package com.pragma.powerup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMessagingMicroserviceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationMessagingMicroserviceApp.class, args);
+    }
+
+}
